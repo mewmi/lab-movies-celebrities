329d14885460bd85d02e099d2c777a27992a919c
joao <njoao99@gmail.com>
2022-10-13 19:52:48 +0100

    dont until iteration 3

--- index.js
+++ index.js
@@ -1,25 +1,57 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 
-const express = require('express');
-const mongoose = require('mongoose');
-const path = require('path');
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const Celebrity = require("./models/celebrity");
 
 const app = express();
 
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res, next) => {
-  res.render('home');
+app.get("/", (req, res, next) => {
+  res.render("home");
+});
+app.get("/celebrities/create", (req, res, next) => {
+  res.render("celebrities/new-celebrity");
+});
+app.post("/celebrities/create", (req, res, next) => {
+  const name = req.body.name;
+  const occupation = req.body.occupation;
+  const catchPhrase = req.body.catchPhrase;
+
+  Celebrity.create({
+    name: name,
+    occupation: occupation,
+    catchPhrase: catchPhrase,
+  })
+    .then(() => {
+      res.redirect("/celebrities");
+    })
+    .catch((error) => {
+      res.redirect("/celebrities/create");
+    });
+});
+
+app.get("/celebrities", (req, res, next) => {
+  const id = req.params.id;
+  Celebrity.findById(id)
+    .then((celebrity) => {
+      res.render("celebrities/celebrities", { celebrity: celebrity });
+    })
+    .catch((error) => {
+      next(error);
+    });
 });
 
 app.use((error, req, res, next) => {
   console.log(error);
-  res.render('error');
+  res.render("error");
 });
 
 mongoose
@@ -28,6 +60,6 @@ mongoose
     app.listen(process.env.PORT);
   })
   .catch((error) => {
-    console.log('There was an error connecting to the database');
+    console.log("There was an error connecting to the database");
     console.log(error);
   });
